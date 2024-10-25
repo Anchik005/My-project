b9f2cf3f76d20ce2d760520763f1daed7a928b25
Anchik005 <Cashire@yandex.ru>
2024-10-25 15:21:57 +0300

    meow

--- Assets/pop.cs
+++ Assets/pop.cs
@@ -1,24 +1,2 @@
-using Cinemachine;
-using System.Buffers;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class pop : MonoBehaviour
-{
-    [SerializeField] private Animator animator;
-    private CinemachineDollyCart docart;
-    private bool played = false;
-    private void Start()
-    {
-        docart = GetComponent<CinemachineDollyCart>();
-    }
-    private void Update()
-    {
-        if (docart.m_Position > 272 && !played)
-        {
-            animator.SetTrigger("ivy");
-            played = true;
-        }
-    }
-}
+
